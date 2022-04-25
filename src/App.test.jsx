0f9ca39da198c355a6a14ab60025aad68ef72a91
m-ghiringhelli <mghiring@gmail.com>
2022-04-25 15:08:42 -0700

    added test for header image

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,24 @@
+import { render, screen } from "@testing-library/react"
+import { MemoryRouter } from "react-router-dom";
+import App from "./App"
+import Header from "./components/layout/Header";
 
-test('Should render the header', () => {
+const user = {
+    id: 1,
+    created_at: '2021-12-13T00:17:29+00:00',
+    name: 'Vonta',
+    avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
+    header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
+    likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
+    motto: 'Res Non Verba',
+    color: 'crimson',
+  }
 
+describe('Header', () => {
+  it('Should contain header image', () => {
+    render(
+      <Header user={user}/>
+    );
+    screen.getByRole('img');
+  });
 })
